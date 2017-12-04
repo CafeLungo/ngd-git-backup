8ae0da806e3c09bcd47708849d859c65a9a6caa6
CafeLungo <CafeLungo@users.noreply.github.com>
2017-12-04 12:59:43 -0500

    Only perform mirror clones

--- ngd-git-backup.sh
+++ ngd-git-backup.sh
@@ -25,19 +25,19 @@ fi
 for REPO in $REPO_LIST; do
   if [[ $REPO != "\"clone_url\":" ]]; then
     REPO=$(echo $REPO | tr -d "\"" | tr -d ",")
-    PROJECT=$(echo $REPO | awk -F '/' '{print $5} ' | awk -F '.' '{print $1} ')
+    PROJECT=$(echo $REPO | awk -F '/' '{print $5} ' | awk -F '.' '{print $1} ').git
     # Clone if the project doesn't exisit locally
     if [ ! -d $PROJECT ]; then
       # cut https:// off to insert <token>@
       REPO=$(echo $REPO | awk -F 'https://' '{print $2} ' )
       echo "Cloning $PROJECT"
       CLONE_URL=$(echo "https://"$TOKEN"@"$REPO)
-      git clone $CLONE_URL
+      git clone --mirror $CLONE_URL
     # Pull / update if the project does exist locally
     elif [ -d $PROJECT ]; then
       cd $PROJECT
       echo "Updating $PROJECT"
-      git pull
+      git fetch --all
       cd $WORK_DIR
     fi
   fi
